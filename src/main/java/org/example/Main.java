1d5dcc556a67e17ee26bac42b1c3c4b3000e81de
Manuel Goyenechea <juanmanuel.goyenechea@paygoal.com.ar>
2024-09-17 08:35:59 -0300

    unsolvedadadd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,18 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+
+
+
+    }
+
+    private static void instanceOf(Object obj) {
+        if (obj instanceof Circulo circulo) {
+            System.out.println("Es un círculo con radio " + circulo.radio());
+        } else if (obj instanceof Cuadrado cuadrado) {
+            System.out.println("Es un cuadrado con lado " + cuadrado.lado());
+        } else if (obj instanceof Triangulo triangulo) {
+            System.out.println("Es un triángulo con base " + triangulo.base() + " y altura " + triangulo.altura());
+        }
     }
 }
